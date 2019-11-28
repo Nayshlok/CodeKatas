57e5b306b10d7a21f4bd3ebe9c9e6dfa4fa7b4a7
Nayshlok <borlandavid@gmail.com>
2019-11-28 00:20:32 -0700

    Snail sort Kata

--- CodeWars/CodeWars/Program.cs
+++ CodeWars/CodeWars/Program.cs
@@ -11,7 +11,28 @@ namespace CodeWars
             //Console.WriteLine(SumOfStrings.sumStrings("12", "123"));
             Console.WriteLine(SumOfStrings.sumStrings("1", "99"));
             Console.WriteLine(SumOfStrings.sumStrings("00091", "030"));
+            //int[][] array =
+            //{
+            //    new []{1, 2, 3},
+            //    new []{4, 5, 6},
+            //    new []{7, 8, 9}
+            //};
 
+            //int[][] array =
+            //{
+            //    new []{1, 2, 3, 4, 5},
+            //    new []{6, 7, 8, 9, 10},
+            //    new []{11, 12, 13, 14, 15},
+            //    new []{16, 17, 18, 19, 20},
+            //    new []{21, 22, 23, 24, 25}
+            //};
+
+            int[][] array =
+            {
+                new []{1},
+            };
+            var sorted = SnailSort.Snail(array);
+            Console.WriteLine(string.Join(", ", sorted));
         }
     }
 }
---
+++ CodeWars/CodeWars/SnailSort.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodeWars
+{
+    public class SnailSort
+    {
+        public static int[] Snail(int[][] array)
+        {
+            int x = 0;
+            int y = 0;
+            int left = 0;
+            int top = 0;
+            int right = array[0].Length - 1;
+            int bottom = array.Length - 1;
+            int step = 0;
+            bool allSame = false;
+            bool isEven = array.Length % 2 == 0;
+            int[] resultarray = new int[array[0].Length * array.Length];
+            if(array[0].Length == 0)
+            {
+                return resultarray;
+            }
+            while (!allSame)
+            {
+                int currentNumber = array[y][x];
+                resultarray[step] = currentNumber;
+                step++;
+
+                if (x < right && y <= top)
+                {
+                    x++;
+                }
+                else if(x >= right && y < bottom)
+                {
+                    y++;
+                }
+                else if(x > left && y >= bottom)
+                {
+                    x--;
+                }
+                else if(x <= left && y > top)
+                {
+                    y--;
+                }
+
+                if (right == left && top == bottom)
+                {
+                    allSame = true;
+                    if (resultarray.Length > 2)
+                    { 
+                        resultarray[step] = array[y][x];
+                        step++;
+                        if (!isEven)
+                        {
+                            x++;
+                            resultarray[step] = array[y][x];
+                        }
+                        else
+                        {
+                            x--;
+                            resultarray[step] = array[y][x];
+                        }
+                    }
+                }
+
+                if (x == right && y == top)
+                {
+                    right--;
+                    if (top < bottom)
+                    {
+                        top++;
+                    }
+                }
+                if (x == left && y == bottom)
+                {
+                    if (left < right)
+                    {
+                        left++;
+                    }
+                    bottom--;
+                }
+            }
+
+            return resultarray;
+        }
+    }
+}
